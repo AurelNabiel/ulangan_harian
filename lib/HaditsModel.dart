6856e4f753f493db84d6907b0cb5886afa8e141d
danen <endra1632@gmail.com>
2021-09-15 10:08:28 +0700

    Hao

---
+++ lib/HaditsModel.dart
@@ -0,0 +1,65 @@
+class Hadits {
+  int number;
+  String arab;
+  String id;
+
+  Hadits({required this.number, required this.arab, required this.id});
+}
+
+var HaditsData = [
+  Hadits(
+      number: 1,
+      arab:
+          "قَالَ حَدَّثَنِي اللَّيْثِيّ عَنْ مَالِك بْن أَنَس عَنْ ابْنِ شِهَابٍ أَنَّ عُمَرَ بْنَ عَبْدِ الْعَزِيزِ أَخَّرَ الصَّلَاةَ يَوْمًا فَدَخَلَ عَلَيْهِ عُرْوَةُ بْنُ الزُّبَيْرِ فَأَخْبَرَهُ أَنَّ الْمُغِيرَةَ بْنَ شُعْبَةَ أَخَّرَ الصَّلَاةَ يَوْمًا وَهُوَ بِالْكُوفَةِ فَدَخَلَ عَلَيْهِ أَبُو مَسْعُودٍ الْأَنْصَارِيُّ فَقَالَمَا هَذَا يَا مُغِيرَةُ أَلَيْسَ قَدْ عَلِمْتَ أَنَّ جِبْرِيلَ نَزَلَ فَصَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ صَلَّى فَصَلَّى رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ثُمَّ قَالَ بِهَذَا أُمِرْتُ فَقَالَ عُمَرُ بْنُ عَبْدِ الْعَزِيزِ اعْلَمْ مَا تُحَدِّثُ بِهِ يَا عُرْوَةُ أَوَ إِنَّ جِبْرِيلَ هُوَ الَّذِي أَقَامَ لِرَسُولِ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ وَقْتَ الصَّلَاةِقَالَ عُرْوَةُ كَذَلِكَ كَانَ بَشِيرُ بْنُ أَبِي مَسْعُودٍ الْأَنْصَارِيُّ يُحَدِّثُ عَنْ أَبِيهِ قَالَ عُرْوَةُ وَلَقَدْ حَدَّثَتْنِي عَائِشَةُ زَوْجُ النَّبِيِّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ أَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ كَانَ يُصَلِّي الْعَصْرَ وَالشَّمْسُ فِي حُجْرَتِهَا قَبْلَ أَنْ تَظْهَرَ",
+      id: "Perawi berkata; Telah menceritakan kepadaku Al Laitsi dari Malik bin Anas dari [Ibnu Syihab]; Suatu hari Umar bin Abdul Aziz pernah mengakhirkan shalat, maka Urwah bin Az Zubair menemuinya dan memberitahukan kepadanya, bahwa suatu hari Mughirah bin Syu'bah mengakhirkan shalat ketika berada di Kufah, sehingga Ibnu Mas'ud menemuinya dan menegurnya; \"Apa maksudmu, hai Mughirah? bukankah kamu tahu, Jibril telah turun kemudian shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat, kemudian dia shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat, kemudian dia shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat juga, kemudian dia shalat dan Rasulullah shallallahu 'alaihi wasallam ikut shalat. Lalu Jibril berkata; \"Seperti ini aku diperintahkan, \" maka Umar bin Abdul Aziz bertanya, \"perhatikanlah apa yang kamu riwayatkan hai Urwah! Apakah Jibril yang mengajarkan waktu shalat untuk Rasulullah shallallahu 'alaihi wasallam?\" [Urwah] menjawab; \" [Basyir bin Mas'ud al Anshari] menceritakan dari [Bapaknya] seperti itu juga\" kemudian [Urwah] menegaskan; dan telah menceritakan kepadaku [Aisyah], istri Nabi shallallahu 'alaihi wasallam, bahwa Rasulullah shallallahu 'alaihi wasallam shalat ashar ketika matahari masih di tempatnya belum tampak. "),
+  Hadits(
+      number: 2,
+      arab:
+          "وَهُوَ الْأَثَرُ الْمَشْهُورُعَنْ رَسُولِ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ مَنْ حَدَّثَ عَنِّي بِحَدِيثٍ يُرَى أَنَّهُ كَذِبٌ فَهُوَ أَحَدُ الْكَاذِبِينَ حَدَّثَنَا أَبُو بَكْرِ بْنُ أَبِي شَيْبَةَ حَدَّثَنَا وَكِيعٌ عَنْ شُعْبَةَ عَنْ الْحَكَمِ عَنْ عَبْدِ الرَّحْمَنِ بْنِ أَبِي لَيْلَى عَنْ سَمُرَةَ بْنِ جُنْدَبٍ ح و حَدَّثَنَا أَبُو بَكْرِ بْنُ أَبِي شَيْبَةَ أَيْضًا حَدَّثَنَا وَكِيعٌ عَنْ شُعْبَةَ وَسُفْيَانَ عَنْ حَبِيبٍ عَنْ مَيْمُونِ بْنِ أَبِي شَبِيبٍ عَنْ الْمُغِيرَةِ بْنِ شُعْبَةَ قَالَا قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ ذَلِكَ",
+      id: "Dan ia merupakan atsar yang masyhur dari Rasulullah shallallahu 'alaihi wasallam, \"Barangsiapa menceritakan hadits dariku, yang mana riwayat itu diduga adalah kebohongan, maka dia (perawi) adalah salah satu dari para pembohong tersebut.\" Telah menceritakan kepada kami [Abu Bakar bin Abu Syaibah] telah menceritakan kepada kami [Waki'] dari [Syu'bah] dari [al Hakam] dari [Abdurrahman bin Abu Laila] dari [Samurah bin Jundab]. (dalam riwayat lain disebutkan) dan juga telah menceritakan kepada kami [Abu Bakar bin Abu Syaibah] telah menceritakan kepada kami [Waki'] dari [Syu'bah] dan [Sufyan] dari [Habib] dari [Maimun bin Abu Syabib] dari [al-Mughirah bin Syu'bah] keduanya berkata, \"Rasulullah shallallahu 'alaihi wasallam bersabda tentang hal tersebut.\""),
+  Hadits(
+      number: 3,
+      arab:
+          "حَدَّثَنَا أَبُو بَكْرِ بْنُ أَبِي شَيْبَةَ قَالَ حَدَّثَنَا شَرِيكٌ عَنْ الْأَعْمَشِ عَنْ أَبِي صَالِحٍ عَنْ أَبِي هُرَيْرَةَ قَالَقَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ مَا أَمَرْتُكُمْ بِهِ فَخُذُوهُ وَمَا نَهَيْتُكُمْ عَنْهُ فَانْتَهُوا",
+      id: "Telah menceritakan kepada kami [Abu Bakar bin Abu Syaibah] berkata; telah menceritakan kepada kami [Syarik] dari [Al A'masy] dari [Abu Shalih] dari [Abu Hurairah] ia berkata; Rasulullah shallallahu 'alaihi wasallam bersabda: \"Apa yang aku perintahkan maka ambillah, dan apa yang aku larang maka tinggalkanlah.\""),
+  Hadits(
+      number: 3,
+      arab:
+          "حَدَّثَنَا عَبْدُ اللَّهِ بْنُ نُمَيْرٍ قَالَ أَخْبَرَنَا إِسْمَاعِيلُ يَعْنِي ابْنَ أَبِي خَالِدٍ عَنْ قَيْسٍ قَالَقَامَ أَبُو بَكْرٍ رَضِيَ اللَّهُ عَنْهُ فَحَمِدَ اللَّهَ وَأَثْنَى عَلَيْهِ ثُمَّ قَالَ يَا أَيُّهَا النَّاسُ إِنَّكُمْ تَقْرَءُونَ هَذِهِ الْآيَةَ{ يَا أَيُّهَا الَّذِينَ آمَنُوا عَلَيْكُمْ أَنْفُسَكُمْ لَا يَضُرُّكُمْ مَنْ ضَلَّ إِذَا اهْتَدَيْتُمْ }وَإِنَّا سَمِعْنَا رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ يَقُولُ إِنَّ النَّاسَ إِذَا رَأَوْا الْمُنْكَرَ فَلَمْ يُنْكِرُوهُ أَوْشَكَ أَنْ يَعُمَّهُمْ اللَّهُ بِعِقَابِهِ",
+      id: "Telah menceritakan kepada kami [Abdullah Bin Numair] dia berkata; telah mengabarkan kepada kami [Isma'il] dari [Qais] dia berkata; [Abu Bakar] berdiri lalu memuji Allah dan mensucikan-Nya kemudian berkata; wahai manusia sesungguhnya kalian membaca ayat ini: \"Hai orang-orang yang beriman, jagalah dirimu; tiadalah orang yang sesat itu akan memberi mudharat kepadamu apabila kamu telah mendapat petunjuk.\" (QS Al Maidah ayat 105). Dan sesungguhnya kami mendengar Rasulullah shallallahu 'alaihi wasallam bersabda: \"sesungguhnya jika manusia melihat kemungkaran kemudian mereka tidak mengingkarinya, maka hampir saja Allah akan menimpakan siksa kepada mereka semua.\""),
+  Hadits(
+      number: 4,
+      arab:
+          "أَخْبَرَنَا قُتَيْبَةُ بْنُ سَعِيدٍ عَنْ مَالِكٍ عَنْ أَبِي الزِّنَادِ عَنْ الْأَعْرَجِ عَنْ أَبِي هُرَيْرَةَأَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ قَالَ لَوْلَا أَنْ أَشُقَّ عَلَى أُمَّتِي لَأَمَرْتُهُمْ بِالسِّوَاكِ عِنْدَ كُلِّ صَلَاةٍ",
+      id: "Telah mengabarkan kepada kami [Qutaibah bin Sa'id] dari [Malik] dari [Abu Zinad] dari [Al A'araj] dari [Abu Hurairah] dia berkata; \"Rasulullah shallallahu 'alaihi wasallam bersabda: \"Seandainya aku tidak khawatir memberatkan umatku, aku pasti memerintahkan mereka untuk bersiwak setiap kali shalat.\" "),
+  Hadits(
+      number: 5,
+      arab:
+          "أَخْبَرَنَا الْحَارِثُ بْنُ مِسْكِينٍ قِرَاءَةً عَلَيْهِ وَأَنَا أَسْمَعُ عَنْ ابْنِ وَهْبٍ عَنْ يُونُسَ عَنْ ابْنِ شِهَابٍ عَنْ سَعِيدِ بْنِ الْمُسَيَّبِ عَنْ أَبِي هُرَيْرَةَعَنْ رَسُولِ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ قَالَ الْفِطْرَةُ خَمْسٌ الِاخْتِتَانُ وَالِاسْتِحْدَادُ وَقَصُّ الشَّارِبِ وَتَقْلِيمُ الْأَظْفَارِ وَنَتْفُ الْإِبْطِ",
+      id: "Telah mengabarkan kepada kami [Al Harits bin Miskin] telah dibacakan kepadanya dan saya mendengarnya dari [Ibnu Wahab] dari [Yunus] dari [Ibnu Syihab] dari [Sa'id bin Musayyab] dari [Abu Hurairah] dari Rasulullah shallallahu 'alaihi wasallam beliau bersabda: \"Lima perkara yang termasuk fitrah, berkhitan, mencukur bulu kemaluan, memotong kumis, memotong kuku, dan mencabut bulu ketiak.\" "),
+  Hadits(
+      number: 6,
+      arab:
+          "أَخْبَرَنَا مُحَمَّدُ بْنُ عَبْدِ الْأَعْلَى قَالَ حَدَّثَنَا الْمُعْتَمِرُ قَالَ سَمِعْتُ مَعْمَرًا عَنْ الزُّهْرِيِّ عَنْ سَعِيدِ بْنِ الْمُسَيَّبِ عَنْ أَبِي هُرَيْرَةَ قَالَقَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ خَمْسٌ مِنْ الْفِطْرَةِ قَصُّ الشَّارِبِ وَنَتْفُ الْإِبْطِ وَتَقْلِيمُ الْأَظْفَارِ وَالِاسْتِحْدَادُ وَالْخِتَانُ",
+      id: "Telah mengabarkan kepada kami [Muhammad bin Abdul A'la] dia berkata; telah menceritakan kepada kami [Al Mu'tamir] berkata; saya mendengar [Ma'mar] dari [Zuhri] dari [Sa'id bin Musayyab] dari [Abu Hurairah] dari Rasulullah shallallahu 'alaihi wasallam beliau bersabda: \"Lima perkara yang termasuk fitrah, memotong kumis, mencabut bulu ketiak, memotong kuku, mencukur bulu kemaluan, dan berkhitan.\" "),
+  Hadits(
+      number: 7,
+      arab:
+          "أَخْبَرَنَا عَلِيُّ بْنُ حُجْرٍ قَالَ أَنْبَأَنَا شَرِيكٌ عَنْ الْمِقْدَامِ بْنِ شُرَيْحٍ عَنْ أَبِيهِ عَنْ عَائِشَةَ قَالَتْمَنْ حَدَّثَكُمْ أَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ بَالَ قَائِمًا فَلَا تُصَدِّقُوهُ مَا كَانَ يَبُولُ إِلَّا جَالِسًا",
+      id: "Telah mengabarkan kepada kami [Ali bin Hujr] dia berkata; Telah memberitakan kepada kami [Syarik] dari [Miqdam bin Syuraih] dari [Ayahnya] dari [Aisyah] dia berkata: \"Barangsiapa mengabarkan kepadamu bahwa Rasulullah shallallahu 'alaihi wasallam buang air kecil sambil berdiri, jangan kamu mempercayainya, karena Rasulullah shallallahu 'alaihi wasallam tidak buang air kecil kecuali sambil duduk.\" "),
+  Hadits(
+      number: 8,
+      arab:
+          "أَخْبَرَنَا أَحْمَدُ بْنُ عَمْرِو بْنِ السَّرْحِ قَالَ أَنْبَأَنَا ابْنُ وَهْبٍ قَالَ أَخْبَرَنِي يُونُسُ عَنْ ابْنِ شِهَابٍ عَنْ أَبِي عُثْمَانَ بْنِ سَنَّةَ الْخُزَاعِيِّ عَنْ عَبْدِ اللَّهِ بْنِ مَسْعُودٍأَنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ نَهَى أَنْ يَسْتَطِيبَ أَحَدُكُمْ بِعَظْمٍ أَوْ رَوْثٍ",
+      id: "Telah mengabarkan kepada kami [Ahmad bin Amr bin As Sarh] dia berkata; telah memberitakan kepada kami [Ibnu Wahab] berkata; telah mengabarkan kepadaku [Yunus] dari [Ibnu Syihab] dari [Abu Utsman bin Sanah Al Khuzaa'i] dari [Abdullah bin Mas'ud] bahwa Rasulullah shallallahu 'alaihi wasallam melarang bersuci dengan tulang atau kotoran hewan.\""),
+  Hadits(
+      number: 9,
+      arab:
+          "و حَدَّثَنَا عُبَيْدُ اللَّهِ بْنُ مُعَاذٍ الْعَنْبَرِيُّ حَدَّثَنَا أَبِي ح و حَدَّثَنَا مُحَمَّدُ بْنُ الْمُثَنَّى حَدَّثَنَا عَبْدُ الرَّحْمَنِ بْنُ مَهْدِيٍّ قَالَا حَدَّثَنَا شُعْبَةُ عَنْ خُبَيْبِ بْنِ عَبْدِ الرَّحْمَنِ عَنْ حَفْصِ بْنِ عَاصِمٍ قَالَقَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ كَفَى بِالْمَرْءِ كَذِبًا أَنْ يُحَدِّثَ بِكُلِّ مَا سَمِعَو حَدَّثَنَا أَبُو بَكْرِ بْنُ أَبِي شَيْبَةَ حَدَّثَنَا عَلِيُّ بْنُ حَفْصٍ حَدَّثَنَا شُعْبَةُ عَنْ خُبَيْبِ بْنِ عَبْدِ الرَّحْمَنِ عَنْ حَفْصِ بْنِ عَاصِمٍ عَنْ أَبِي هُرَيْرَةَ عَنْ النَّبِيِّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ بِمِثْلِ ذَلِكَ",
+      id: "Dan telah menceritakan kepada kami [Ubaidullah bin Mu'adz al Anbari] telah menceritakan kepada kami [Bapakku] (dalam riwayat lain disebutkan), Dan telah menceritakan kepada kami [Muhammad bin al Mutsanna] telah menceritakan kepada kami [Abdurrahman bin Mahdi] keduanya berkata, telah menceritakan kepada kami [Syu'bah] dari [Khubaib bin Abdurrahman] dari [Hafsh bin Ashim] dia berkata, \"Rasulullah shallallahu 'alaihi wasallam bersabda: \"Cukuplah seseorang (dianggap) berbohong apabila dia menceritakan semua yang dia dengarkan.\" Telah menceritakan kepada kami [Abu Bakar bin Abu Syaibah] telah menceritakan kepada kami [Ali bin Hafsh] telah menceritakan kepada kami [Syu'bah] dari [Khubaib bin Abdurrahman] dari [Hafsh bin 'Ashim] dari [Abu Hurairah] dari Nabi shallallahu 'alaihi wasallam dengan seperti hadits tersebut.\""),
+  Hadits(
+      number: 10,
+      arab:
+          "حَدَّثَنَا عَفَّانُ حَدَّثَنَا شُعْبَةُ عَنْ أَبِي إِسْحَاقَ قَالَ سَمِعْتُ عَمْرَو بْنَ مَيْمُونٍ قَالَصَلَّى بِنَا عُمَرُ بِجَمْعٍ الصُّبْحَ ثُمَّ وَقَفَ وَقَالَ إِنَّ الْمُشْرِكِينَ كَانُوا لَا يُفِيضُونَ حَتَّى تَطْلُعَ الشَّمْسُ وَإِنَّ رَسُولَ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ خَالَفَهُمْ ثُمَّ أَفَاضَ قَبْلَ أَنْ تَطْلُعَ الشَّمْسُ",
+      id: "Telah menceritakan kepada kami ['Affan] Telah menceritakan kepada kami [Syu'bah] dari [Abu Ishaq] dia berkata; aku mendengar ['Amru Bin Maimun] berkata; [Umar] shalat shubuh bersama kami di Muzdalifah kemudian berdiri dan berkata; \"Sesungguhnya orang-orang musyrik tidak meninggalkan Muzdalifah sampai matahari terbit dan sesungguhnya Rasulullah shallallahu 'alaihi wasallam telah menyelisihi mereka, sehingga beliau meninggalkan Muzdalifah sebelum matahari terbit.\"")
+];
