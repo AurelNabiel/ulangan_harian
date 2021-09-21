57c24a33d44ceb756f26c2b33dea6cb904aa9f4c
unknown <danifudail@gmail.com>
2021-09-21 21:31:19 +0700

    ulangan

--- lib/detailquran.dart
+++ lib/detailquran.dart
@@ -42,17 +42,7 @@ class AyatTampil extends StatelessWidget {
                 ),
               ],
             ),
-<<<<<<< HEAD
-            body: ListView.builder(
-              itemCount: ayat.ayat.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                    child: Container(
-                        height: 90,
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10),
-                        decoration: BoxDecoration(
-=======
+
              body: ListView.builder(
                    itemCount: ayat.ayat.length,
                    itemBuilder: (context, index) {   
@@ -60,7 +50,7 @@ class AyatTampil extends StatelessWidget {
                 return Container(
                         height: 350,
                          decoration: BoxDecoration(
->>>>>>> 74507612aaff84ff0d5bb097f9594e7004ce1c12
+
                             borderRadius:
                                 BorderRadius.all(Radius.circular(25.0)),
                             color: Colors.white,
@@ -125,31 +115,10 @@ class AyatTampil extends StatelessWidget {
                                   ],
                                 ),
                               ),
-<<<<<<< HEAD
-                              Expanded(
-                                flex: 2,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text(
-                                        ayat.ayat[index].translation_aya_text,
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 32.0,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'Oxygen'),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-=======
->>>>>>> 74507612aaff84ff0d5bb097f9594e7004ce1c12
-                              ),
+
                              
-                            ],
+                              ),
+                            ]
                           ),
                         ));
               },
