51ec18d6f5a5f9f13a1589e03bcd258942f100dc
unknown <danifudail@gmail.com>
2021-09-22 09:19:54 +0700

    ulangan

--- lib/detailhadits.dart
+++ lib/detailhadits.dart
@@ -4,20 +4,17 @@ import 'package:ulangan_harian/main.dart';
 import 'package:ulangan_harian/listhadits.dart';
 import 'package:ulangan_harian/model/HaditsModel.dart';
 
-
 class HaditsTampil extends StatelessWidget {
-
   final Hadits arab;
   HaditsTampil({required this.arab});
 
-  
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             backgroundColor: Color(0xFF21BFBD),
             appBar: AppBar(
-              title:  Center(
+              title: Center(
                 child: Text(
                   "Daftar Surat",
                   textAlign: TextAlign.right,
@@ -45,82 +42,57 @@ class HaditsTampil extends StatelessWidget {
                 ),
               ],
             ),
-             body: ListView.builder(
-              itemCount: haditsdata.length,
-              itemBuilder: (context, index) {
-               
-                return Container(
-                        height: 400,
-                         decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(25.0)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.black.withAlpha(300),
-                                  blurRadius: 10.0),
-                            ]),
-                        // margin: const EdgeInsets.symmetric(
-                        //     horizontal: 10, vertical: 10),
-                       
-                        child: Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-
-                               
-
-                                   
-                                
-                             
-                              Expanded(
-                                
-                                flex: 2,
-                                child: Padding(
-                                  padding: EdgeInsets.all(4.0),
-                                child: Column(
-                                  children: [
-
-                                      
-                                    Text(
-                                      arab.arab,
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 20.0,
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'Oxygen'),
-                                          textAlign: TextAlign.right,
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text(
-                                      arab.id,
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 18.0,
-                                          fontFamily: 'Oxygen'),
-                                          textAlign: TextAlign.left,
-                                    ),
-                                  ],
-
+            body: SingleChildScrollView(
+              child: Container(
+                  height: 2800,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  // decoration: BoxDecoration(
+                    
+                  //     borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  //     color: Colors.white,
+                  //     boxShadow: [
+                  //       BoxShadow(
+                  //           color: Colors.black.withAlpha(300), blurRadius: 10.0),
+                  //     ]),
+                  child: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Padding(
+                                    padding: EdgeInsets.all(10.0),
+                              child: Column(children: [
+                                Text(
+                                  arab.arab,
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Oxygen'),
+                                  textAlign: TextAlign.right,
                                 ),
-                              ),
-                              ),
-                             
-                            ],
-                          ),
-                        ));
-              },
-            )
-             
-             )
-        );
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  arab.id,
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.0,
+                                      fontFamily: 'Oxygen'),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ])
+                          )
+                              )
+                        ],
+                      ))),
+            )));
   }
-
-
-  }
+}
 
 
 // var information Text( 
